9a0e7df9ebfeb884666d052f5c9bf159a7b3209a
Tristan Bridges <mophead4@gmail.com>
2024-04-10 13:41:32 -0500

    Formatting

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -1,37 +1,36 @@
-console.log('****** Part Supply *******');
+console.log("****** Part Supply *******");
 // ***** REQUIRED FEATURES *********************************************
 // For each question, be sure to use console.log statements to ensure that
 // your code does what you expect!
 
 // 1. Create a variable called 'partsNeeded' with a value of the number 40.
-console.log('1. Create partsNeeded:');
+console.log("1. Create partsNeeded:");
 let partsNeeded = 40;
-console.log('Needed Parts:', partsNeeded);
-
+console.log("Needed Parts:", partsNeeded);
 
 // 2. Create a variable called 'supplyChanges' whose value is an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11.
-console.log('2. Create supplyChanges:');
-let supplyChanges = [3,5,-6,0,7,11];
-console.log('Supply Changes:', supplyChanges);
+console.log("2. Create supplyChanges:");
+let supplyChanges = [3, 5, -6, 0, 7, 11];
+console.log("Supply Changes:", supplyChanges);
 
 // 3. Create a variable called 'secondItem' and assign it the value of the second
 //    item in the 'supplyChanges' array.
-console.log('3. Access the second value of supplyChanges:');
+console.log("3. Access the second value of supplyChanges:");
 let secondItem = supplyChanges[1];
-console.log('Second Item:', secondItem);
+console.log("Second Item:", secondItem);
 
 // 4. The last value in the 'supplyChanges' array was added by mistake.
 //    Remove it from the array and store it inside a new variable called 'removedItem'.
-console.log('4. Remove the last value from supplyChanges:');
+console.log("4. Remove the last value from supplyChanges:");
 let removedItem = supplyChanges.pop();
-console.log('Removed Item:', removedItem);
+console.log("Removed Item:", removedItem);
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
-console.log('5. Add the value 25 into supplyChanges.');
+console.log("5. Add the value 25 into supplyChanges.");
 let pushedParts = 25;
 supplyChanges.push(pushedParts);
 console.log(supplyChanges);
-console.log('Pushed amount of parts:', supplyChanges[supplyChanges.length -1]);
+console.log("Pushed amount of parts:", supplyChanges[supplyChanges.length - 1]);
 
 // 6. Create three new variables named 'positives', 'negatives', and
 //    'zeroes' whose values are empty arrays. Then, write a for loop
@@ -40,61 +39,61 @@ console.log('Pushed amount of parts:', supplyChanges[supplyChanges.length -1]);
 //      - If the value is a positive number, push it into the 'positives' array.
 //      - If the value is a negative number, push it into the 'negatives' array.
 //      - If the value is a zero, push it into the 'zeroes' array.
-console.log('6. Looping through supplyChanges to populate arrays with positive, negative, and zero values:');
+console.log(
+  "6. Looping through supplyChanges to populate arrays with positive, negative, and zero values:"
+);
 let positives = [];
 let negatives = [];
 let zeroes = [];
-for (i=0; i <= supplyChanges.length; i++) {
-  if (supplyChanges[i] > 0){
+for (i = 0; i <= supplyChanges.length; i++) {
+  if (supplyChanges[i] > 0) {
     positives.push(supplyChanges[i]);
-  } 
-  if (supplyChanges[i] < 0){
-    negatives.push(supplyChanges[i]);
   }
-  else if (supplyChanges[i] === 0){
+  if (supplyChanges[i] < 0) {
+    negatives.push(supplyChanges[i]);
+  } else if (supplyChanges[i] === 0) {
     zeroes.push(supplyChanges[i]);
   }
 }
-console.log('Positives:', positives);
-console.log('Negatives:', negatives);
-console.log('Zeroes:', zeroes);
-
-
+console.log("Positives:", positives);
+console.log("Negatives:", negatives);
+console.log("Zeroes:", zeroes);
 
 // ***** STRETCH GOALS *********************************************
 // 7. Rewrite the 'for' loop from #6 as a 'for...of' loop. Instead of 'positives',
 //    'negatives', and 'zeroes', create three new arrays named 'stretchPositives',
-//    'stretchNegatives', and 'stretchZeroes'. 
-console.log('7. Looping through supplyChanges to populate more arrays with positive, negative, and zero values:');
+//    'stretchNegatives', and 'stretchZeroes'.
+console.log(
+  "7. Looping through supplyChanges to populate more arrays with positive, negative, and zero values:"
+);
 let stretchPositives = [];
 let stretchNegatives = [];
 let stretchZeroes = [];
 for (let sign of supplyChanges) {
-  if (sign > 0){
-    stretchPositives.push(sign)
+  if (sign > 0) {
+    stretchPositives.push(sign);
   }
   if (sign < 0) {
-    stretchNegatives.push(sign)
-  }
-  else if (sign === 0){
-    stretchZeroes.push(sign)
+    stretchNegatives.push(sign);
+  } else if (sign === 0) {
+    stretchZeroes.push(sign);
   }
 }
-console.log('stretchPositives',stretchPositives);
-console.log('stretchNegatives',stretchNegatives);
-console.log('stretchZeroes',stretchZeroes);
+console.log("stretchPositives", stretchPositives);
+console.log("stretchNegatives", stretchNegatives);
+console.log("stretchZeroes", stretchZeroes);
 
 // 8. Create a variable called 'totalParts' and assign it a value of 0.
 //    Then, write a loop that adds each value of the 'supplyChanges'
 //    array to the 'totalParts' variable.
-console.log('8. Looping through supplyChanges to calculate the sum:');
+console.log("8. Looping through supplyChanges to calculate the sum:");
 let totalParts = 0;
-for (let part of supplyChanges){
+for (let part of supplyChanges) {
   totalParts += part;
   console.log(totalParts);
 }
-console.log('Total Parts:', totalParts);
-// 9. We have a large stash of parts in our warehouse that we 
+console.log("Total Parts:", totalParts);
+// 9. We have a large stash of parts in our warehouse that we
 //    need to box up and get ready for shipment. There are 572
 //    parts in total, and each box holds 7 parts.
 //    Create two variables:
@@ -108,34 +107,36 @@ console.log('Total Parts:', totalParts);
 console.log('9. Filling boxes with a "while" loop');
 let parts = 572;
 let boxesFilled = 0;
-while (boxesFilled != 81){
+while (boxesFilled != 81) {
   parts -= 7;
   boxesFilled += 1;
   // console.log(parts,boxesFilled);
 }
-console.log('Parts remaining:', parts);
-console.log('Boxes filled:', boxesFilled);
-
-
+console.log("Parts remaining:", parts);
+console.log("Boxes filled:", boxesFilled);
 
 // DO NOT MODIFY
 // Used for automated testing
 try {
   module.exports = {
-    partsNeeded: typeof partsNeeded !== 'undefined' ? partsNeeded : undefined,
-    supplyChanges: typeof supplyChanges !== 'undefined' ? supplyChanges : undefined,
-    secondItem: typeof secondItem !== 'undefined' ? secondItem : undefined,
-    removedItem: typeof removedItem !== 'undefined' ? removedItem : undefined,
-    positives: typeof positives !== 'undefined' ? positives : undefined,
-    negatives: typeof negatives !== 'undefined' ? negatives : undefined,
-    zeroes: typeof zeroes !== 'undefined' ? zeroes : undefined,
-    stretchPositives: typeof stretchPositives !== 'undefined' ? stretchPositives : undefined,
-    stretchNegatives: typeof stretchNegatives !== 'undefined' ? stretchNegatives : undefined,
-    stretchZeroes: typeof stretchZeroes !== 'undefined' ? stretchZeroes : undefined,
-    totalParts: typeof totalParts !== 'undefined' ? totalParts : undefined,
-    parts: typeof parts !== 'undefined' ? parts : undefined,
-    boxesFilled: typeof boxesFilled !== 'undefined' ? boxesFilled : undefined,
-  }
+    partsNeeded: typeof partsNeeded !== "undefined" ? partsNeeded : undefined,
+    supplyChanges:
+      typeof supplyChanges !== "undefined" ? supplyChanges : undefined,
+    secondItem: typeof secondItem !== "undefined" ? secondItem : undefined,
+    removedItem: typeof removedItem !== "undefined" ? removedItem : undefined,
+    positives: typeof positives !== "undefined" ? positives : undefined,
+    negatives: typeof negatives !== "undefined" ? negatives : undefined,
+    zeroes: typeof zeroes !== "undefined" ? zeroes : undefined,
+    stretchPositives:
+      typeof stretchPositives !== "undefined" ? stretchPositives : undefined,
+    stretchNegatives:
+      typeof stretchNegatives !== "undefined" ? stretchNegatives : undefined,
+    stretchZeroes:
+      typeof stretchZeroes !== "undefined" ? stretchZeroes : undefined,
+    totalParts: typeof totalParts !== "undefined" ? totalParts : undefined,
+    parts: typeof parts !== "undefined" ? parts : undefined,
+    boxesFilled: typeof boxesFilled !== "undefined" ? boxesFilled : undefined,
+  };
 } catch (e) {
   // Do nothing
 }
